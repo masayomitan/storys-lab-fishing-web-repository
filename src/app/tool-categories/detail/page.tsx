b01984acb8ee8a7d397472c8d48dade9cea9304f
kawasaki <yomitanmasa3523@gmail.com>
2024-03-16 22:22:06 +0900

    戻し

--- src/app/tool-categories/detail/page.tsx
+++ src/app/tool-categories/detail/page.tsx
@@ -1,24 +1,24 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { Container } from '@chakra-ui/react';
-// import { useParams } from "next/navigation";
+import { Container } from '@chakra-ui/react';
+import { useParams } from "next/navigation";
 
 
-// const ToolCategory = () => {
-//   const [toolCategoryId, setToolCategoryId] = useState<string | null>(null);
+const ToolCategory = () => {
+  const [toolCategoryId, setToolCategoryId] = useState<string | null>(null);
 
-//   const params = useParams();
+  const params = useParams();
 
-//   useEffect(() => {
-//     if (params && params.id) {
-//       const toolCategoryId = params.id
-//       setToolCategoryId(toolCategoryId as string);
-//     } 
-//   }, [params]);
+  useEffect(() => {
+    if (params && params.id) {
+      const toolCategoryId = params.id
+      setToolCategoryId(toolCategoryId as string);
+    } 
+  }, [params]);
 
-//   return (
-//     <Container>道具種別詳細ページ {toolCategoryId}</Container>
-//   );
-// }
+  return (
+    <Container>道具種別詳細ページ {toolCategoryId}</Container>
+  );
+}
 
-// export default ToolCategory;
+export default ToolCategory;
